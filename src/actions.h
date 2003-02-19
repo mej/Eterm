15ad73f445c576d26b51e6c7aa8bde15e0585353
Michael Jennings <mej@kainx.org>
2003-02-19 21:18:10 +0000

    Wed Feb 19 16:17:00 2003                        Michael Jennings (mej)
    Updated copyright statements for 2003.
    
    
    SVN revision: 6698

--- src/actions.h
+++ src/actions.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1997-2002, Michael Jennings
+ * Copyright (C) 1997-2003, Michael Jennings
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
--- src/options.c
+++ src/options.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 1997-2002, Michael Jennings
+ * Copyright (C) 1997-2003, Michael Jennings
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to
@@ -351,7 +351,7 @@ static void
 usage(void)
 {
     printf("Eterm Enlightened Terminal Emulator for the X Window System\n");
-    printf("Copyright (c) 1997-2002, " AUTHORS "\n\n");
+    printf("Copyright (c) 1997-2003, " AUTHORS "\n\n");
 
     printf("Option types:\n");
     printf("  (bool) -- Boolean option ('1', 'on', 'yes', or 'true' to activate, '0', 'off', 'no', or 'false' to deactivate)\n");
@@ -377,7 +377,7 @@ version(void)
 {
 
     printf("Eterm " VERSION "\n");
-    printf("Copyright (c) 1997-2002, " AUTHORS "\n\n");
+    printf("Copyright (c) 1997-2003, " AUTHORS "\n\n");
 
     printf("Build info:\n");
     printf("    Built on " BUILD_DATE "\n");
